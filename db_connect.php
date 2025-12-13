dc5cf99cf98d2de5be441cb326f98df39de7ff77
jowvik <jowvik>
2025-12-13 20:33:04 +0700

    edit name

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,7 @@
 <?php
-    $koneksi = mysqli_connect('localhost','root','','student_db');
+    $koneksi = mysqli_connect('localhost','root','','Anginbnb_Database');
 
     if(mysqli_connect_errno()){
         echo'koneksi database gagal : ' . mysqli_connect_error();
     }
-
-
-
 ?>
